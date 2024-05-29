c96dff28359f8d1b1563f04b6f4452f5501773ad
bob <bobmcwishon@gmail.com>
2024-05-29 14:09:25 +0200

    created initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+ module.exports = {
+   mode: 'development',
+   entry: './src/index.js',
+   output: {
+    filename: '[name].js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   devtool: 'inline-source-map',
+   devServer: {
+    static: './dist',
+   },
+   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+      clean: true,
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+ };
