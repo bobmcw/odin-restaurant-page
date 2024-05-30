d3f1cb3da11feb0dc4510b5f61b41f34e0f89446
bob <bobmcwishon@gmail.com>
2024-05-30 20:58:17 +0200

    restructured creating buttons and added menu button

--- src/header.js
+++ src/header.js
@@ -1,18 +1,24 @@
 import renderHome from "./home";
+import renderMenu from "./menu";
 
 const renderPage = (page) => { 
     const content = document.querySelector('#content');
     content.innerHTML = "";
     content.appendChild(page);
 }
+const pageRenderButton = (name,page) => {
+    const button = document.createElement("button");
+    button.classList.add("navbutton");
+    button.innerText = `${name}`;
+    button.addEventListener("click",() =>{
+        renderPage(page);
+    });
+    return button;
+    
+}
 
 export default function renderHeader(){
     const header = document.querySelector("header");
-    const home = document.createElement("button");
-    home.classList.add("navbutton");
-    home.innerText = "Home";
-    home.addEventListener("click",() =>{
-        renderPage(renderHome());
-    });
-    header.appendChild(home);
+    header.appendChild(pageRenderButton("home",renderHome())); 
+    header.appendChild(pageRenderButton("Menu",renderMenu()));
 }
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+
+export default function renderMenu(){
+const test = document.createElement("h1");
+test.innerText = "test!";
+return test
+}
