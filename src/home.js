a90ac4ec1cfc57f530ac78c88da3cef761548d7a
bob <bobmcwishon@gmail.com>
2024-05-30 14:44:18 +0200

    added a list of opening/closing hours

--- src/home.js
+++ src/home.js
@@ -11,16 +11,36 @@ const galery = () => {
         galeryObject.appendChild(image);
     });    
     return galeryObject;
-    }
+    };
+const createDay = (day,opening,closing) =>{
+    const line = document.createElement("p");
+    line.innerText = `${day}: ${opening}am - ${closing}pm`
+    return line;
+}
+const info = () =>{
+    const infobody = document.createElement("div");
+    const header = document.createElement("h2");
+    header.innerText = "Hours we are open!";
+    infobody.appendChild(header);
+    infobody.appendChild(createDay("Sunday",8,8));
+    infobody.appendChild(createDay("Monday",8,6));
+    infobody.appendChild(createDay("Tuesday",8,6));
+    infobody.appendChild(createDay("Wednesday",8,6));
+    infobody.appendChild(createDay("Thursday",8,6));
+    infobody.appendChild(createDay("Friday",8,9));
+    infobody.appendChild(createDay("Saturday",8,9));
+    return infobody; 
+}
 
 export default function renderHome() {
     const homeBody = document.createElement("div");
     homeBody.classList.add("home");
 
     const header = document.createElement("h1");
-    header.innerText = "Welcome to my kitchen!";
+    header.innerText = "Welcome to our kitchen!";
     homeBody.appendChild(header);
     homeBody.appendChild(galery())
+    homeBody.appendChild(info())
 
     return homeBody;
 }
