d477ef2091b20cefd6293db9a5d9242381fa8bcb
bob <bobmcwishon@gmail.com>
2024-05-31 12:59:58 +0200

    added more contend and changed the styling

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,9 @@
-import img1 from './images/1.jpeg';
+import coffee from './images/coffee.jpeg';
+import tea from './images/tea.jpeg'
+import lemonade from './images/lemonade.jpeg'
 const menuItem = (itemname,description,imagesrc) => {
     const item = document.createElement("div");
+    item.classList.add("menuItem");
     const header = document.createElement("h4");
     header.innerText = itemname;
     const desc = document.createElement("p");
@@ -14,11 +17,15 @@ const menuItem = (itemname,description,imagesrc) => {
 }
 export default function renderMenu(){
     const menuBody = document.createElement("div");
-    const beverageHeader = document.createElement("h3");
+    menuBody.classList.add("menu");
+    const beverageHeader = document.createElement("h2");
     const beverages = document.createElement("div");
+    beverages.classList.add("category")
     beverageHeader.innerText = "Beverages";
     beverages.appendChild(beverageHeader);
-    beverages.appendChild(menuItem("coffee","a tasty black coffee to start your day off!",img1));
+    beverages.appendChild(menuItem("coffee","a tasty black coffee to start your day off!",coffee));
+    beverages.appendChild(menuItem("tea","a fresh tea with ginger, lemon and mint, a perfect fit for your breakfast!",tea));
+    beverages.appendChild(menuItem("lemonade","a fresh, homemade lemonade made from fresh lemons, ice cold prefect for a hot day",lemonade));
     menuBody.appendChild(beverages);
     return menuBody;
 }
